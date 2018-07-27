baf318ad168d7a854b5242153c920f797ecd80cf
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-07-26 22:22:29 -0500

    Initial commit

---
+++ Assets/Juego/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    private GameObject wayPoint;
+    private Vector3 wayPointPos;
+    //This will be the zombie's speed. Adjust as necessary.
+    private float speed = 4.0f;
+    void Start()
+    {
+        //At the start of the game, the zombies will find the gameobject called wayPoint.
+        wayPoint = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        wayPointPos = new Vector3(wayPoint.transform.position.x, transform.position.y, wayPoint.transform.position.z);
+        //Here, the zombie's will follow the waypoint.
+        transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed * Time.deltaTime);
+    }
+}
