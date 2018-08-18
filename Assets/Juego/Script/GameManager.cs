1388888a8a568657401d423885cf7313c387b09e
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-08-18 11:43:30 -0500

    Estetica UI
    Se genero un boton de disparo estetico con el ambiente, ademas se cambio la mecanica de supervivencia, es tiempo ilimitado y se basa en el mayor tiempo de duracion, se guarda el highscore.

--- Assets/Juego/Script/GameManager.cs
+++ Assets/Juego/Script/GameManager.cs
@@ -8,41 +8,64 @@ public class GameManager : MonoBehaviour {
 
     public Text score;
     public GameObject lose;
+    public Text Highscore;
     private int scoreCoins;
+    private int highscore;
     public Text tiempoText;
-    private float tiempo = 40;
+    private float tiempo = 0;
     public Text finalScore;
-
+    public Text tiempoFinal;
+    private bool muerto;
 
     public static GameManager Instance { set; get; }
 
     void Start () {
         Time.timeScale = 1;
         Instance = this;
-        score.text = "Score: " + scoreCoins.ToString("0");
+        score.text = "Puntaje: " + scoreCoins.ToString("0");
     }
 
     private void FixedUpdate()
     {
-        tiempo -= Time.fixedDeltaTime;
+        if(muerto == false)
+        {
+            tiempo += Time.fixedDeltaTime;
+        }
+
         tiempoText.text = "Segundos: " + tiempo.ToString("0");
 
-        if (tiempo <= 0)
-        {
-            Loser();
-        }
+        //if (tiempo <= 0)
+        //{
+        //    Loser();
+        //    tiempo = 0;
+        //    Time.timeScale = 0;
+        //}
     }
 
     public void GetCoin()
     {
         scoreCoins++;
-        score.text = "Score: " + scoreCoins.ToString("0");
+        score.text = "Puntaje: " + scoreCoins.ToString("0");
     }
 
     public void Loser()
     {
+        muerto = true;
         lose.SetActive(true);
-        finalScore.text = "Final Score: " + scoreCoins.ToString("0");
+        finalScore.text = "Puntaje final: " + scoreCoins.ToString("0");
+        tiempoFinal.text = "Tiempo: " + tiempo.ToString("0");
+
+        highscore = PlayerPrefs.GetInt("highscore");
+        Highscore.text = "Maximo Puntaje: " + highscore.ToString("0");
+
+        if (scoreCoins > highscore)
+        {
+            highscore = scoreCoins;
+            Highscore.text = "Maximo Puntaje: " + highscore.ToString("0");
+
+            PlayerPrefs.SetInt("highscore", highscore);
+        }
+
     }
 
     public void Resetiar()
