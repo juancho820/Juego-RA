3c07a9a926f7900d86e84b7e4b74d35c7c534b31
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-09-23 12:34:28 -0500

    Union de todos los juegos
    Se huzo la union de los 4 juegos de realidad aumentada en unos solo Hockey, Topos, Survival y lego mas un lobby para escoger los juegos

---
+++ Assets/Lego/Script/UIManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public RectTransform colorMenu;
+    public RectTransform actionMenu;
+
+    private bool menuAnimating;
+    private bool areMenusShowing;
+    private float menuAnimationTransition;
+    private float animationDuration = 0.2f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            OnTheOneButtonClick();
+        }
+
+        if (menuAnimating)
+        {
+            if (areMenusShowing)
+            {
+                menuAnimationTransition += Time.deltaTime * (1 - animationDuration);
+                if(menuAnimationTransition >= 1)
+                {
+                    menuAnimationTransition = 1;
+                    menuAnimating = false;
+                }
+            }
+            else
+            {
+                menuAnimationTransition -= Time.deltaTime;
+                if (menuAnimationTransition <= 0)
+                {
+                    menuAnimationTransition = 0;
+                    menuAnimating = false;
+                }
+            }
+
+            colorMenu.anchoredPosition = Vector2.Lerp(new Vector2(0, -50), new Vector2(0,100), menuAnimationTransition);
+            actionMenu.anchoredPosition = Vector2.Lerp(new Vector2(-200, 0), new Vector2(50, 0), menuAnimationTransition);
+        }
+    }
+
+    public void OnTheOneButtonClick()
+    {
+        areMenusShowing = !areMenusShowing;
+        PlayMenuAnimation();
+    }
+
+    private void PlayMenuAnimation()
+    {
+        menuAnimating = true;
+    }
+}
